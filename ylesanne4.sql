ea153cc0bf3426bf0d31a02c4850621fad69a59a
Risto Hinno <ristohinno@gmail.com>
2016-09-25 18:58:35 +0300

    update

--- ylesanne4.sql
+++ ylesanne4.sql
@@ -1,13 +1,29 @@
 --1. Leida klubi ‘Laudnikud’ liikmete nimekiri (eesnimi, perenimi) tähestiku järjekorras.
 SELECT eesnimi, perenimi 
 FROM Isik, Klubi
+WHERE Klubi.id=Isik.klubi
+AND Klubi.nimi= 'Laudnikud'
+ORDER BY perenimi ASC, 
+eesnimi ASC;
+--teine variant
+SELECT eesnimi, perenimi 
+FROM Isik
+JOIN Klubi
+ON Klubi.id=Isik.klubi
 WHERE Klubi.nimi= 'Laudnikud'
 ORDER BY perenimi ASC, 
 eesnimi ASC;
 --2. Leida klubi ‘Laudnikud’ liikmete arv.
 SELECT COUNT(*) AS 'Liikmete arv'
-FROM Isik, Klubi
+FROM Isik
+JOIN Klubi
+ON Klubi.id=Isik.klubi
 WHERE Klubi.nimi= 'Laudnikud'
+--teine variant
+SELECT COUNT(*) AS 'Liikmete arv'
+FROM Isik, Klubi
+WHERE Klubi.id=Isik.klubi
+AND Klubi.nimi= 'Laudnikud'
 --3.Leida V-tähega algavate klubide M-tähega algavate eesnimedega isikute perekonnanimed (ja ei muud).
 SELECT perenimi 
 FROM Isik 
@@ -22,15 +38,16 @@ FROM Isik, Klubi
 WHERE Isik.klubi= Klubi.id 
 	AND(LEFT(Klubi.nimi,1)= 'V' 
 	AND LEFT(Isik.eesnimi,1)= 'M');
+--kolmas variant
+SELECT perenimi 
+FROM Isik
+JOIN Klubi
+	ON Isik.klubi= Klubi.id 
+	AND(LEFT(Klubi.nimi,1)= 'V' 
+	AND LEFT(Isik.eesnimi,1)= 'M');
 --4. Leida kõige esimesena alanud partii algusaeg.
 SELECT MIN(algushetk) AS algusaeg
 FROM Partii;
---jäi segaseks kas tahetakse kellaaega koos kuupäevaga või mitte, ilma kuupäevata on  (NB antud juhul on kaks partiid ühe algusajaga)
-SELECT CONVERT(TIME,Algushetk)
- FROM partii 
- WHERE algushetk = (
-	SELECT MIN(algushetk) 
-	FROM partii);
 --5. Leida partiide mängijad (väljad: valge ja must), mis algasid 04. märtsil ajavahemikus 9:00 kuni 11:00.
 SELECT valge, must 
 FROM Partii 
@@ -55,13 +72,21 @@ WHERE EXISTS(
 	AND Datediff(minute, algushetk, lopphetk) 
 	BETWEEN 9 AND 11 
 	AND valge_tulemus=2);
---eelmine andis unikaalsed nimed, see iga oartii kohta eraldi
+--eelmine andis unikaalsed nimed, see iga partii kohta eraldi
 SELECT eesnimi, perenimi 
 FROM Isik, Partii
 WHERE Isik.Id= Partii.valge 
 	AND Datediff(minute, algushetk, lopphetk) 
 	BETWEEN 9 AND 11 
 	AND valge_tulemus=2
+--sama, mis eelmine tulemus, kuid JOINiga
+SELECT eesnimi, perenimi 
+FROM Isik 
+JOIN Partii 
+	ON Isik.Id= Partii.valge 
+	AND Datediff(minute, algushetk, lopphetk) 
+	BETWEEN 9 AND 11 
+	AND valge_tulemus=2;
 --7. Leida rohkem kui 1 kord esinevad perekonnanimed (ja ei muud).
 SELECT perenimi 
 FROM Isik 
@@ -85,6 +110,12 @@ SELECT COUNT(*) AS 'Arvod valgetega partiid'
 FROM Partii, Isik 
 WHERE Partii.valge= Isik.id 
 	AND Isik.eesnimi='Arvo';
+--kolmas võimalus -JOIN
+SELECT COUNT(*) AS 'Arvod valgetega partiid'
+FROM Partii 
+JOIN Isik 
+ON Partii.valge= Isik.id 
+	AND Isik.eesnimi='Arvo';
 --10. Leida kõigi Arvode poolt valgetega mängitud partiide arv turniiride lõikes.
 SELECT Turniir.nimi, COUNT(*) AS 'Arvod valgetega partiid' 
 FROM Partii 
@@ -93,6 +124,14 @@ WHERE EXISTS(
 	SELECT* FROM Isik 
 	WHERE Partii.valge= Isik.id 
 	AND Isik.eesnimi='Arvo') 
+GROUP BY nimi;
+--või JOINiga
+SELECT Turniir.nimi, COUNT(*) AS 'Arvod valgetega partiid' 
+FROM Partii 
+KEY JOIN Turniir 
+JOIN Isik 
+	ON Partii.valge= Isik.id 
+	AND Isik.eesnimi='Arvo' 
 GROUP BY nimi;
 --11. Leida kõigi Mariade mustadega mängitud mängudest saadud punktide arv (tulemus = 2 on võit ja annab 1 punkti, 
 --tulemus = 1 on viik ja annab pool punkti).
@@ -106,6 +145,16 @@ WHERE EXISTS(
 	SELECT* FROM Isik 
 	WHERE Partii.must= Isik.id 
 	AND isik.Eesnimi='Maria'); 
+--või JOINiga
+SELECT SUM(
+	CASE WHEN musta_tulemus=2 THEN 1 
+	WHEN musta_tulemus=1 THEN 0.5 
+	ELSE musta_tulemus END) 
+AS 'musta tulemus' 
+FROM Partii 
+JOIN Isik 
+	ON Partii.must= Isik.id 
+	AND isik.Eesnimi='Maria'; 
 --12. Leida partiide keskmine kestvus turniiride kaupa (tulemuseks on tabel 2 veeruga: turniiri nimi, keskmine partii pikkus) 
 SELECT Turniir.nimi AS 'turniiri nimi', 
 	AVG(Datediff(minute, algushetk, lopphetk)) AS 'keskmine partii pikkus' 
