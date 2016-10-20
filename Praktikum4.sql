3021640b2ac1307f27d5ab9b9c7d6855568e0b7e
Risto Hinno <ristohinno@gmail.com>
2016-10-20 07:48:49 +0300

    uue praksi ja ülesande asjad

---
+++ Praktikum4.sql
@@ -0,0 +1,73 @@
+--Deterministlik funktsioon
+--Funktsioon kahe arvu liitmiseks:
+CREATE FUNCTION f_liida(a_arv1 integer,a_arv2 integer) 
+RETURNS INTEGER
+DETERMINISTIC
+BEGIN 
+DECLARE summa INTEGER;
+SET summa = a_arv1 + a_arv2;
+RETURN summa;
+END;
+--Funktsioon, mis vastavalt id väärtusele tagastab eesnime 
+CREATE FUNCTION f_eesnimi(a_id integer)
+RETURNS varchar(50) 
+NOT DETERMINISTIC
+BEGIN
+DECLARE d_enimi varchar(50);
+SELECT eesnimi INTO d_enimi
+FROM isik
+WHERE id = a_id;
+RETURN d_enimi;
+END;
+--kasutamne
+SELECT f_eesnimi(83);
+SELECT f_eesnimi(83) 
+FROM sys.dummy;
+SELECT id, f_eesnimi(id) 
+FROM isik; --ääretult ebaoptimaalne!!
+--Protseduur, mis lisab tabelisse  klubi uue klubi ja väljastab vastava teate:
+CREATE PROCEDURE sp_uus_klubi(IN a_nimi VARCHAR (100), 
+IN a_asukoht VARCHAR(100),
+OUT a_id INTEGER)
+BEGIN
+DECLARE i_id  INTEGER;
+INSERT INTO klubi(nimi, asukoht)
+VALUES (a_nimi, a_asukoht);
+SELECT @@identity  INTO i_id;
+--teade kliendile
+MESSAGE 'Uus klubi: ' || i_id TO CLIENT;
+SET a_id= i_id;
+END;
+--Meil on vaja muutujat, kuhu salvestatakse uue klubi id
+--•Pöördume protseduuri poole
+--•aatame milline on uue klubi id väärtus 
+CREATE VARIABLE uusid INTEGER;
+CALL sp_uus_klubi('Valga Valge', 'Valga',uusid);
+SELECT uusid;
+--Protseduur, mille väljundiks on klubimängijate tabel:
+CREATE PROCEDURE sp_klubimangija(IN a_klubi_id INTEGER)
+RESULT (eesnimi VARCHAR(50), perenimi VARCHAR(50), kuupaev DATE)
+BEGIN 
+SELECT eesnimi, perenimi, CURRENT DATE
+FROM isik
+WHERE klubi = a_klubi_id
+ORDER BY eesnimi;
+END
+--kasutamine
+CALL sp_klubimangija(51);
+CALL sp_klubimangija(a_klubi_id=51);
+--(parameetri väärtustamine nime järgi)
+EXECUTE sp_klubimangija(51);
+SELECT * FROM sp_klubimangija (51);
+--(kasutamine päringus)
+
+--indeksid
+CREATE INDEX  ix_algus ON partii(algushetk DESC)
+CREATE INDEX ix_nimi ON  isik(perenimi ASC, eesnimi ASC)
+
+
+
+
+
+
+
