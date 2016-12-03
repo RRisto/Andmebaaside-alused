cfe090de728e34c6da77e13e76d16e88d6dbe3b5
Risto Hinno <ristohinno@gmail.com>
2016-12-03 22:04:34 +0200

    update

--- pileti_ylesanne.sql
+++ pileti_ylesanne.sql
@@ -1,11 +1,12 @@
 --ÜLESANNE: leida andmebaasist ope isik, kes konkreetsel turniiril kulutas kõige rohkem aega
---algne versioon, turniiri 47 kõige rohkem aeg kulutanud isiku nimi
+--algne versioon, turniiril 47 kõige rohkem aeg kulutanud isiku nimi
 select top 1 eesnimi+' '+  perenimi as nimi from (
 select valge as isik, sum(datediff(minute, algushetk, lopphetk)) as aeg from partii where turniir=47 group by isik
 union
 select must as isik, sum(datediff(minute, algushetk, lopphetk)) as aeg from partii where turniir=47 group by isik
 ) as tabel join isik on isik.id=tabel.isik
 group by nimi order by sum(aeg) desc;
+
 --funktsioonina (anna ainult turniiri id ette)
 CREATE FUNCTION f_pikim_aeg(turniir_id integer)
 RETURNS varchar(50) 
